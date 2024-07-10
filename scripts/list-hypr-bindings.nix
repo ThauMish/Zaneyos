66618c8053a203dcb8faf1aa76e579802d51e03b
ThauMish <MjA0MjEwMWFjMWY2ZT2YzNjcyZTZkN2M@proton.me>
2024-07-10 10:34:51 +0200

    [CLEANUP] Clean some useless thingys

--- scripts/list-hypr-bindings.nix
+++ scripts/list-hypr-bindings.nix
@@ -1,5 +1,9 @@
 { pkgs, host, ... }:
 
+let
+  inherit (import ../hosts/${host}/variables.nix) terminal browser;
+in
+
 pkgs.writeShellScriptBin "list-hypr-bindings" ''
   yad --width=800 --height=650 \
   --center \
